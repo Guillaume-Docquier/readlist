b816e69ec11379c2f96b6987ab629de723262228
Guillaume Docquier <guillaumedocquier@hotmail.com>
2025-01-12 10:56:02 -0500

    Added eslint-config-love for TS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import neostandard, { resolveIgnoresFromGitignore } from "neostandard";
+import love from "eslint-config-love";
 import globals from "globals";
 
 export default [
@@ -9,5 +10,17 @@ export default [
     noJsx: true,
     ignores: resolveIgnoresFromGitignore(),
   }),
+  {
+    ...love,
+    rules: {
+      ...love.rules,
+      "@typescript-eslint/no-magic-numbers": [
+        "error",
+        { ignoreNumericLiteralTypes: true },
+      ],
+    },
+    files: ["**/*.ts"],
+    ignores: ["rsbuild.config.ts"],
+  },
   { linterOptions: { reportUnusedDisableDirectives: true } },
 ];
